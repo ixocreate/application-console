402e0ce7c0428408702de615d0fe85f4c3a052b4
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2018-03-15 15:19:03 +0100

    CS

--- src/Factory/CommandInitializer.php
+++ src/Factory/CommandInitializer.php
@@ -1,5 +1,14 @@
 <?php
+/**
+ * kiwi-suite/application-console (https://github.com/kiwi-suite/application-console)
+ *
+ * @package kiwi-suite/application-console
+ * @see https://github.com/kiwi-suite/application-console
+ * @copyright Copyright (c) 2010 - 2018 kiwi suite GmbH
+ * @license MIT License
+ */
 
+declare(strict_types=1);
 namespace KiwiSuite\ApplicationConsole\Factory;
 
 use KiwiSuite\Contract\ServiceManager\InitializerInterface;
